14440d36a0d04550ff54e02b76737ad774ce4dda
Killian Morin <killiancham@gmail.com>
2022-11-21 15:40:16 +0100

    creation du makefile et du .h pour printf

---
+++ printf/libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmorin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 14:44:13 by kmorin            #+#    #+#             */
+/*   Updated: 2022/11/21 15:22:15 by kmorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+//# include "libft.h"
+
+#endif
