447fa5eb63a81808661307f4b3515fb38d10696a
Killian Morin <killiancham@gmail.com>
2023-03-13 15:18:23 +0100

    fractol fait un makefile avec les regles classiques (ajouter les framework et les library qd compil et cree le binaire), fait un .h avec les headers des fonctions autorisees et le header de la mlx, mis la mlx

---
+++ fract_ol/includes/fract_ol.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fract_ol.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmorin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 11:21:27 by kmorin            #+#    #+#             */
+/*   Updated: 2023/03/13 11:24:00 by kmorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACT_OL_H
+# define FRACT_OL_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <math.h>
+# include "../mlx/mlx.h"
+
+#endif
