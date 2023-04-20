a2ea65f95e8b59c40fcdeb54135a9347bc9bf1e7
Killian Morin <killiancham@gmail.com>
2023-04-20 15:13:33 +0200

    add pipex

---
+++ pipex/srcs/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmorin <kmorin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/20 09:45:27 by kmorin            #+#    #+#             */
+/*   Updated: 2023/04/20 09:45:00 by kmorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 5)
+	{
+	}
+	else
+	{
+		ft_putstr_fd("Sorry, not enough parameters\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	return (0);
+}
