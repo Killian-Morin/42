7ea6a4bb7a20211c4f9711418d556ba1f5736a10
Killian Morin <killiancham@gmail.com>
2023-05-12 15:54:02 +0200

    gnl bonus corriger -> mis le bon nom de header dans les 2 _bonus.c

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*gnl_update_stock(char *stock)
 {
