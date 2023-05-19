0f11015e472d7c2593556ee176913129bf525f8f
Killian Morin <killiancham@gmail.com>
2023-05-19 16:20:55 +0200

    start of philo: creation of Makefile, philo.h and main.c

---
+++ philo/includes/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmorin <kmorin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 13:30:44 by kmorin            #+#    #+#             */
+/*   Updated: 2023/05/19 13:37:33 by kmorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
