1c3afc7a23941560e81547fb48f9485d6ea709fe
Killian Morin <killiancham@gmail.com>
2023-05-22 15:45:52 +0200

    creer 3 struct, creer main, check_arg, get_time (pas sur si marche), init_data(marche pas -> segfault a l'initialization des variables en faisant un ft_atoi des argv

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: kmorin <kmorin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 13:30:44 by kmorin            #+#    #+#             */
-/*   Updated: 2023/05/19 13:37:33 by kmorin           ###   ########.fr       */
+/*   Updated: 2023/05/22 15:40:27 by kmorin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,47 @@
 # include <unistd.h>
 # include <sys/time.h>
 # include <pthread.h>
+
+/*
+memset, printf, malloc, free, write, usleep, gettimeofday
+pthread_create, pthread_detach, pthread_join,
+pthread_mutex_init, pthread_mutex_destroy,pthread_mutex_lock,
+pthread_mutex_unlock
+*/
+
+typedef struct s_time {
+	int			meal_to_eat;
+	int			die_time;
+	int			eat_time;
+	int			sleep_time;
+	long int	start_time;
+}				t_time;
+
+typedef struct s_philo {
+	int			num;
+	int			state;
+	int			meal_status;
+	long int	time_last_meal;
+	t_time		*time;
+	struct s_philo		*next;
+	struct s_philo		*prev;
+	struct s_table		*table;
+}				t_philo;
+
+typedef struct s_table {
+	int		philo_dead;
+	int		nbr_philo;
+	t_time	*time;
+	t_philo	*philo_prime;
+}			t_table;
+
+/*  main.c  */
+int			main(int ac, char **av);
+void		init_data(int ac, char **av, t_philo *p);
+int			check_args(int ac, char **av);
+long int	get_time(void);
+
+/*  ft_atoi */
+int			ft_atoi(char *str);
 
 #endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,8 +6,65 @@
 /*   By: kmorin <kmorin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 13:33:17 by kmorin            #+#    #+#             */
-/*   Updated: 2023/05/19 13:34:42 by kmorin           ###   ########.fr       */
+/*   Updated: 2023/05/22 15:43:26 by kmorin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+int	check_args(int ac, char **av)
+{
+	if (ac != 5 && ac != 6)
+	{
+		printf("Sorry not the right amount of arguments\n");
+		return (1);
+	}
+	else if (ft_atoi(av[1]) < 1)
+	{
+		printf("Sorry not a valid amount of philosopher\n");
+		return (1);
+	}
+	else
+		return (0);
+}
+
+long int	get_time(void)
+{
+	struct timeval	tmp;
+	long int		time;
+	long int		second;
+	long int		microsecond;
+
+	gettimeofday(&tmp, NULL);
+	second = tmp.tv_sec * 1000;
+	microsecond = tmp.tv_usec / 1000;
+	time = second + microsecond;
+	return (time);
+}
+
+void	init_data(int ac, char **av, t_philo *p)
+{
+	p->table->nbr_philo = ft_atoi(av[1]);
+	p->time->die_time = ft_atoi(av[2]);
+	p->time->eat_time = ft_atoi(av[3]);
+	p->time->sleep_time = ft_atoi(av[4]);
+	if (ac == 6)
+		p->time->meal_to_eat = ft_atoi(av[5]);
+	printf("%d\n", p->table->nbr_philo);
+	printf("%d\n", p->time->die_time);
+	printf("%d\n", p->time->eat_time);
+	printf("%d\n", p->time->sleep_time);
+	p->time->start_time = get_time();
+}
+
+int	main(int ac, char **av)
+{
+	t_philo	*p;
+
+	if (check_args(ac, av) == 1)
+		return (1);
+	p = malloc(sizeof(p));
+	if (!p)
+		return (1);
+	init_data(ac, av, p);
+}
