b3e6346ec135143a55030fd8e6bdf6605bbdb255
Killian Morin <killiancham@gmail.com>
2023-12-29 16:49:22 +0100

    cpp09/ex01 seems finished

---
+++ cpp/cpp09/ex01/RPN.cpp
@@ -0,0 +1,178 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmorin <kmorin@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 16:54:18 by kmorin            #+#    #+#             */
+/*   Updated: 2023/12/29 16:44:09 by kmorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RPN.hpp"
+
+/* ************************************************************************** */
+/*                            CANONICAL FUNCTIONS                             */
+/* ************************************************************************** */
+
+RPN::RPN(void) {
+
+	if (SHOWMSG)
+		std::cout << COLOR("Default constructor called", GREEN) << std::endl;
+}
+
+RPN::RPN(const RPN& src) {
+
+	this->_container = src._container;
+
+	if (SHOWMSG)
+		std::cout << COLOR("Copy constructor called", GREEN) << std::endl;
+}
+
+RPN&	RPN::operator=(const RPN& rhs) {
+
+	if (this != &rhs)
+		this->_container = rhs._container;
+
+	if (SHOWMSG)
+		std::cout << COLOR("Assignation operator called", GREEN) << std::endl;
+
+	return (*this);
+}
+
+RPN::~RPN(void) {
+
+	if (SHOWMSG)
+		std::cout << COLOR("Defaut destructor called", RED) << std::endl;
+}
+
+/* ************************************************************************** */
+/*                              MEMBERS FUNCTIONS                             */
+/* ************************************************************************** */
+
+bool	RPN::checker(const std::string& s) const {
+
+	if (s.empty() || s.find_first_not_of(' ') == s.npos) {
+		std::cout << COLOR("Error: expression is empty :(", RED) << std::endl;
+		return (false);
+	}
+
+	if (s.find_first_not_of("0123456789 +-/*") != s.npos) {
+		std::cout << COLOR("Error: expression contains invalid characters.", RED) << std::endl;
+		return (false);
+	}
+
+	if (s.find_first_of("+-/*") == s.npos) {
+		std::cout << COLOR("Error: no operator in the expression.", RED) << std::endl;
+		return (false);
+	}
+
+	if (s.length() < 5) {
+		std::cout << COLOR("Error: expression is invalid (too short).", RED) << std::endl;
+		return (false);
+	}
+
+	if (s[s.length() - 1] != '+' && s[s.length() - 1] != '-' && s[s.length() - 1] != '*' && s[s.length() - 1] != '/') {
+		std::cout << COLOR("Error: operator must be the last character of expression.", RED) << std::endl;
+		return (false);
+	}
+
+	if (s[0] == '+' || s[0] == '-' || s[0] == '*' || s[0] == '/' || s[0] == ' ') {
+		std::cout << COLOR("Error: number must be the first character of expression.", RED) << std::endl;
+		return (false);
+	}
+
+	//since spaces will always be on a even index
+	for (size_t i = 0; i < s.length(); i++) {
+		if (i % 2 == 0) {
+			if (!isdigit(s[i]) && s[i] != '+' && s[i] != '-' && s[i] != '*' && s[i] != '/') {
+				std::cout << COLOR("Error: expression is invalid (digit or operator not in correct place).", RED) << std::endl;
+				return (false);
+			}
+			if (isdigit(s[i + 1])) {
+				std::cout << COLOR("Error: expression contains a number > 10.", RED) << std::endl;
+				return (false);
+			}
+		}
+		else {
+			if (s[i] != ' ') {
+				std::cout << COLOR("Error: expression is invalid (space not in correct place).", RED) << std::endl;
+				return (false);
+			}
+		}
+	}
+
+	return (true);
+}
+
+void	RPN::run(const std::string& s) {
+
+	size_t i = 0;
+	while (i < s.length()) {
+
+		if (s[i] == ' ') {
+			i++;
+			continue;
+		}
+
+		if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/') {
+			if (this->_container.size() < 2) {
+				std::cout << COLOR("Error: not enough numbers or operands.", RED) << std::endl;
+
+				while (!this->_container.empty())
+					this->_container.pop();
+
+				return ;
+			}
+
+			int	rhs = this->_container.top();
+			this->_container.pop();
+			int	lhs = this->_container.top();
+			this->_container.pop();
+
+			if (s[i] == '+')
+				this->_container.push(lhs + rhs);
+			else if (s[i] == '-')
+				this->_container.push(lhs - rhs);
+			else if (s[i] == '*')
+				this->_container.push(lhs * rhs);
+			else if (s[i] == '/')
+				this->_container.push(lhs / rhs);
+
+		}
+		else
+			this->_container.push(myStoi(&s[i]));
+
+		i++;
+	}
+
+	if (this->_container.size() > 1) {
+		std::cout << COLOR("Error: too many numbers or operands.", RED) << std::endl;
+
+		while (!this->_container.empty())
+			this->_container.pop();
+
+		return ;
+	}
+	std::cout << COLOR(this->_container.top(), MAGENTA) << std::endl;
+}
+
+int	RPN::myStoi(const std::string& s) {
+
+	int	i;
+	std::stringstream	ss(s);
+
+	ss >> i;
+
+	if (ss.fail()) {
+		std::cout << COLOR("Error: conversion failed.", RED) << std::endl;
+
+		while (!this->_container.empty())
+			this->_container.pop();
+
+		exit(EXIT_FAILURE);
+	}
+
+	return (i);
+}
---
+++ cpp/cpp09/ex01/RPN.hpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmorin <kmorin@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 16:54:26 by kmorin            #+#    #+#             */
+/*   Updated: 2023/12/29 16:22:52 by kmorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <stack>
+
+#ifndef SHOWMSG
+# define SHOWMSG 0
+#endif
+
+#define RESET "\e[0m"
+#define RED "\e[0;31m"
+#define GREEN "\e[0;32m"
+#define YELLOW "\e[0;33m"
+#define BLUE "\e[0;34m"
+#define MAGENTA "\e[0;35m"
+#define CYAN "\e[0;36m"
+#define WHITE "\e[0;37m"
+
+#define COLOR(text, color) color << text << RESET
+
+class RPN {
+
+	private:
+		std::stack<int>	_container;
+
+	public:
+		//Canonical class functions
+		RPN(void);
+		RPN(const RPN& src);
+		RPN&	operator=(const RPN& rhs);
+		~RPN(void);
+
+		//Member function
+		bool	checker(const std::string& s) const;
+
+		void	run(const std::string& s);
+
+		int		myStoi(const std::string& s);
+
+};
+
+#endif
